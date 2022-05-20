23998213ef720f132806881d1d88b00a893fa603
mygetright-junhaw <junhaw@getright.my>
2022-05-20 08:03:55 +0800

    third/branch first commit

--- firstfile.php
+++ firstfile.php
@@ -10,5 +10,7 @@ another change from second branch
 3rd change from second branch
 to test fast forward
 
-now try again no ff commit. 
+now try again no ff commit.
+
+added thrid branch and first commit 
  ?>
